3a90cffc08bf5481849419fc2f422bb4fc81acf2
maximastashkin <maksim.astash@gmail.com>
2022-05-24 11:09:28 +0300

    lab7 has done

--- lab7/functions.js
+++ lab7/functions.js
@@ -9,3 +9,104 @@ function f2(x) {
 function f3(x) {
     return Math.exp(x - 2) + Math.pow(x, 3) + 2 * x * Math.log(x + 3) / 7;
 }
+
+function getMin(a, b, h, f) {
+    let min = Infinity;
+    let tabPredicate = getTabulationPredicate(a, b);
+    for (let i = a; tabPredicate(i, b, h); i += h) {
+        let value = f(i);
+        if (Number.isFinite(value) && value < min) {
+            min = value;
+        }
+    }
+    return min;
+}
+
+getMin.stringRepresentation = "Minimum value";
+
+function isMonotonicallyDecreases(a, b, h, f) {
+    let tabPredicate = getTabulationPredicate(a, h > 0 ? b - h : b + h);
+    for (let i = 0; tabPredicate(i, b, h); i += h) {
+        if (f(i + 1) > f(i)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+isMonotonicallyDecreases.stringRepresentation = "Is monotonically decreases";
+
+function findNullValuesCount(a, b, h, f) {
+    let count = 0;
+    let tabPredicate = getTabulationPredicate(a, b);
+    for (let i = 0; tabPredicate(i, b, h); i += h) {
+        if (f(i) === 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+findNullValuesCount.stringRepresentation = "Null values count";
+
+function getMemoizedFunction(f) {
+    const cache = {};
+    let memoizedFunction = (x) => {
+        if (x in cache) {
+            return cache[x];
+        }
+        let value = f(x);
+        cache[x] = value;
+        return value;
+    };
+    memoizedFunction.getFromCache = (x) => {
+        return cache[x];
+    }
+    memoizedFunction.getCacheSize = () => {
+        return Object.keys(cache).length;
+    }
+
+    clone(f, memoizedFunction);
+    return memoizedFunction;
+}
+
+function getDebuggingFunction(f) {
+    let debuggingFunc = (x) => {
+        let value = f(x);
+        console.log(`${new Date()} f(${x}) = ${value}`)
+        return value;
+    };
+    clone(f, debuggingFunc);
+    return debuggingFunc;
+}
+
+function getFunctionWithCallsCount(f) {
+    let callsCount = 0;
+    let functionWithCallsCount = (x) => {
+        callsCount++;
+        return f(x);
+    }
+    functionWithCallsCount.getCallsCount = () => {
+        return callsCount;
+    }
+
+    functionWithCallsCount.resetCallsCount = () => {
+        callsCount = 0;
+    }
+    clone(f, functionWithCallsCount);
+    return functionWithCallsCount;
+}
+
+function clone(src, dst) {
+    for (let key in src) {
+        dst[key] = src[key];
+    }
+}
+
+function getTabulationPredicate(x0, xn) {
+    if (x0 < xn) {
+        return (x, xn, hx) => x <= (xn + hx / 3);
+    } else {
+        return (x, xn, hx) => x >= (xn - hx / 3);
+    }
+}
--- lab7/index.js
+++ lab7/index.js
@@ -5,9 +5,28 @@ const hInputId = "hValue";
 const outputDivId = "output";
 const errorDivId = "error";
 
-function calculate() {
+const f1RadioId = "f1";
+const f2RadioId = "f2";
+const f3RadioId = "f3";
+
+const minimumCheckBoxId = "minimum";
+const isMonotonicallyDecreasesCheckBoxId = "isMonotonicallyDecreases";
+const zeroValuesCountCheckBoxId = "zeroValuesCount";
+
+const memoizationCheckBoxId = "memoization";
+const debuggingCheckBoxId = "debugging";
+const callsCountCheckBoxId = "callsCount";
+
+const callsCountDivId = "callsCountDiv"
+
+function clearAll() {
     clearOutput();
     clearError();
+    clearCallsCount();
+}
+
+function calculate() {
+    clearAll();
     let a = getInputValue(aInputId);
     let b = getInputValue(bInputId);
     let h = getInputValue(hInputId);
@@ -15,14 +34,20 @@ function calculate() {
         addErrorMessage("Tabulating params aren't valid! Try again");
         return;
     }
+    let f = getFunctionsVariant(getFunction());
+    let results = calculateFunctionCharacteristics(a, b, h, f, getCharacteristicsFunctions());
+    appendToOutput(getResultTable(results));
 
+    if (f.hasOwnProperty("getCallsCount")) {
+        setCallsCount(f.getCallsCount());
+    }
 }
 
 function getInputValue(inputId) {
     const input = document.getElementById(inputId);
     let value = Number(input.value);
     input.value = value;
-    input.setAttribute("value", value);
+    input.setAttribute("value", value.toString());
     return value;
 }
 
@@ -34,6 +59,17 @@ function addErrorMessage(message) {
     getErrorDiv().appendChild(
         document.createTextNode(message)
     );
+}
+
+function clearCallsCount() {
+    getCallsCountDiv().innerHTML = "";
+}
+
+function setCallsCount(callsCount) {
+    clearCallsCount();
+    getCallsCountDiv().appendChild(document.createTextNode(
+        `Function calls count: ${callsCount}`
+    ));
 }
 
 function clearError() {
@@ -50,4 +86,87 @@ function clearOutput() {
 
 function getOutputDiv() {
     return document.getElementById(outputDivId);
+}
+
+function getCallsCountDiv() {
+    return document.getElementById(callsCountDivId);
+}
+
+function getFunction() {
+    if (isChecked(f1RadioId)) {
+        return f1;
+    }
+    if (isChecked(f2RadioId)) {
+        return f2;
+    }
+    if (isChecked(f3RadioId)) {
+        return f3;
+    }
+}
+
+function getCharacteristicsFunctions() {
+    let functions = [];
+    if (isChecked(minimumCheckBoxId)) {
+        functions.push(getMin);
+    }
+    if (isChecked(isMonotonicallyDecreasesCheckBoxId)) {
+        functions.push(isMonotonicallyDecreases);
+    }
+    if (isChecked(zeroValuesCountCheckBoxId)) {
+        functions.push(findNullValuesCount)
+    }
+    return functions;
+}
+
+function getFunctionsVariant(f) {
+    if (isChecked(memoizationCheckBoxId)) {
+        f = getMemoizedFunction(f);
+    }
+    if (isChecked(debuggingCheckBoxId)) {
+        f = getDebuggingFunction(f);
+    }
+    if (isChecked(callsCountCheckBoxId)) {
+        f = getFunctionWithCallsCount(f);
+    }
+    return f;
+}
+
+function calculateFunctionCharacteristics(a, b, h, f, characteristics) {
+    let result = [];
+    characteristics.forEach(func => {
+        result.push({
+            name: func.stringRepresentation,
+            characteristic: func(a, b, h, f)
+        });
+    })
+    return result;
+}
+
+function isChecked(checkBoxId) {
+    return document.getElementById(checkBoxId).checked;
+}
+
+function getResultTable(results) {
+    let table = document.createElement("table");
+    let tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+    results.forEach(result => tbody.appendChild(
+        getTrByCharacteristic(result)
+    ));
+    return table;
+}
+
+function getTrByCharacteristic(characteristic) {
+    let tr = document.createElement("tr");
+    let titleTd = document.createElement("td");
+    titleTd.appendChild(document.createTextNode(characteristic.name));
+    tr.appendChild(titleTd);
+    let valueTd = document.createElement("td");
+    valueTd.appendChild(document.createTextNode(characteristic.characteristic));
+    tr.appendChild(valueTd);
+    return tr;
+}
+
+function appendToOutput(element) {
+    getOutputDiv().appendChild(element);
 }
